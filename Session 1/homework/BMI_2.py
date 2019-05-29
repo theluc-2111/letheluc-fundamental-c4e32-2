95215aa1b4611b6f00003431acbdf4d52fddb1aa
theluc-2111 <letheluc.aof@gmail.com>
2019-05-29 15:46:14 +0700

    initial commit

--- Session 1/homework/BMI_2.py
+++ Session 1/homework/BMI_2.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    body = {'weight':61,'height':169}
-    BMI = body['weight']/((body['height']/100)**2)
+@app.route('/<weight>/<height>')
+def index(weight,height):
+    BMI = int(weight)/((int(height)/100)**2)
     return render_template('idex_BMI.html', BMI = BMI)
 
 if __name__ == '__main__':
